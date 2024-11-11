af21d0e3a5c6f32452a5e240219ca7664c3ddbb5
maxius-k <kv170123@gmail.com>
2024-11-11 15:15:33 +0300

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,14 @@
+#1st program
+print((9*5)**0,5)
+
+#2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd program
+print(2+2*2)
+print((2+2)*2)
+print(2+2*2 == (2+2)*2)
+
+#4th program
+print(int(float('123.456')*10)-1230) #мой вариант решения
+print(int(float('123.456') * 10)%10) # смотрел предложенное решение в ответах
