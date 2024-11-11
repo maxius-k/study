eb68ded301a2419c0179570b74789ffa33b1dd45
maxius-k <kv170123@gmail.com>
2024-11-11 15:08:12 +0300

    homework1

---
+++ main.py
@@ -0,0 +1,33 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Hello, World")
+
+
+#1st program
+print((9*5)**0,5)
+
+#2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd program
+print(2+2*2)
+print((2+2)*2)
+print(2+2*2 == (2+2)*2)
+
+#4th program
+print(int(float('123.456')*10)-1230) #мой вариант решения
+print(int(float('123.456') * 10)%10) # смотрел предложенное решение в ответах
+
+
