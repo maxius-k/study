e671a6fc328be53a80f344ffd34ce0691430c5ff
maxius-k <kv170123@gmail.com>
2024-11-13 13:15:25 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,5 @@
+completed_homework = 12
+hours = 1.5
+course_name = 'python'
+time = (hours / completed_homework)
+print('Курс:', course_name,', всего задач:', completed_homework,', затрачено часов:', hours,', среднее время выполнения', time, 'часа')
